e467f2beb87789618ec3ea3a89693fd3f1059ab9
Deathblood <deathbloodjr@gmail.com>
2025-02-06 11:57:49 -0600

    Moved classes to their own files.
    I was getting confused, so hopefully this is less confusing.

---
+++ NijiiroScoring/Plugins/SongDataPoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NijiiroScoring.Plugins
+{
+    public class SongDataPoints
+    {
+        public int Points { get; set; }
+        public int ScoreRank { get; set; }
+    }
+}
